d3e1bb7fff3ec04c47b21c01f1f10c923829d06a
summaiya14 <summaiyamahmod@gmail.com>
2020-04-03 14:51:57 -0700

    minor changes

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import {useEffect, useState} from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+
+const [results, setResults] = useState([]);
+const [errorMsg, setErrorMsg] = useState('');
+
+const searchApi = async searchTerm => {
+console.log('Food app');
+try{
+	const response = await yelp.get('/search', {
+		params: {
+			limit: 50,
+			term: searchTerm,
+			location: 'san jose'
+		}
+	});
+	setResults(response.data.businesses);
+	} catch (err) {
+		setErrorMsg('Something went wrong');
+	}
+};
+
+useEffect(() => {
+	searchApi('pasta');
+}, []);
+
+return [searchApi, results, errorMsg];
+
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,36 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
 const [term, setTerm] = useState('');
-const [results, setResults] = useState([]);
-const [errorMsg, setErrorMsg] = useState('');
-
-const searchApi = async () => {
-try{
-	const response = await yelp.get('/search', {
-		params: {
-			limit: 50,
-			term,
-			location: 'san jose'
-		}
-	});
-	setResults(response.data.businesses);
-	} catch (err) {
-		setErrorMsg('Something went wrong');
-	}
-};
+const [searchApi, results, errorMsg] = useResults();
 
   return <View>
   <SearchBar
   term = {term}
   onTermChange = {setTerm}
-  onTermSubmit={searchApi}
+  onTermSubmit={() => searchApi(term)}
   />
   {errorMsg ? <Text>{errorMsg}</Text> : null}
   <Text>We have found {results.length} results!</Text>
+  <ResultsList title='Cost Effective'/>
+  <ResultsList title='Bit Pricier'/>
+  <ResultsList title='Big Spender'/>
+
   </View>
 };
 
