31533eb1aaee45c9259aea37fcacf5a5237a9bb5
summaiya14 <summaiyamahmod@gmail.com>
2020-04-05 10:34:06 -0700

    fetching images

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsDetail from '../components/ResultsDetail';
 
+
 const ResultsList = ({title, results, navigation}) => {
+if (!results.length){
+	return null;
+}
 
 return <View style={styles.container}> 
 <Text style={styles.title}>{title}</Text>
@@ -13,7 +18,7 @@ data={results}
 keyExtractor={(result) => result.id}
 renderItem={({ item }) => {
 return (
-<TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+<TouchableOpacity onPress={() => navigation.navigate('ResultsShow',{id: item.id})}>
 <ResultsDetail result={item}/>
 </TouchableOpacity>
 )
@@ -36,6 +41,6 @@ container: {
 }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
 
 
